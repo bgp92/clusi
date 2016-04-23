b3edda2b1e4b7dfdc84ae76606cfc9ee7a46ce53
Benji <bpg_92@hotmail.com>
2016-04-23 03:38:26 -0400

    cha cha cha chaanges

--- config/env/development.js
+++ config/env/development.js
@@ -17,18 +17,6 @@ module.exports = {
     // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
     format: 'dev',
     options: {
-      // Stream defaults to process.stdout
-      // Uncomment/comment to toggle the logging to a log on the file system
-      // stream: {
-      //  directoryPath: process.cwd(),
-      //  fileName: 'access.log',
-      //  rotatingLogs: { // for more info on rotating logs - https://github.com/holidayextras/file-stream-rotator#usage
-      //    active: false, // activate to use rotating logs
-      //    fileName: 'access-%DATE%.log', // if rotating logs are active, this fileName setting will be used
-      //    frequency: 'daily',
-      //    verbose: false
-      //  }
-      // }
     }
   },
   app: {
